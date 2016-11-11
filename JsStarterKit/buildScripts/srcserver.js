a13bc3aea32cf299222f5a019515fecaeeae3182
Marina Neyman <marina.neyman@opuscms.com>
2016-11-11 14:41:15 -0600

    installed node.js

---
+++ JsStarterKit/buildScripts/srcserver.js
@@ -0,0 +1,11 @@
+﻿var express = required("express");
+var path = required("path");
+var open = required("open");
+
+var port = 3000;
+
+var app = express();
+
+app.get('/', function(req, res) {
+    
+});
