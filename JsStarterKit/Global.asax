47a82007a0829c31bbc972c1d00d4a3b9b1e8cd5
Marina Neyman <marina.neyman@opuscms.com>
2016-11-11 13:58:09 -0600

    first commit

---
+++ JsStarterKit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JsStarterKit.MvcApplication" Language="C#" %>
---
+++ JsStarterKit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JsStarterKit.Startup))]
+namespace JsStarterKit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
